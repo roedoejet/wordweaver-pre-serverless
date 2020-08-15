2dcc7aac62332f8f87dbfd361450b5a64b9cdc06
roedoejet <aidanpine@shaw.ca>
2020-08-15 10:51:14 -0700

    fix: added nginx init to init script

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -10,7 +10,7 @@ domains=($DOMAIN)
 rsa_key_size=4096
 data_path="./volumes/ssl/certbot"
 email=$LE_EMAIL # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -32,7 +32,7 @@ echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
 docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:1024 -days 1\
+  openssl req -x509 -nodes -newkey rsa:4096 -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
@@ -40,6 +40,7 @@ echo
 
 
 echo "### Starting nginx ..."
+docker-compose -f docker-compose.prod.yml up --force-recreate -d backend # Backend also needs to start in order to build nginx config properly
 docker-compose -f docker-compose.prod.yml up --force-recreate -d reverse
 echo
 
